7e9fcaa6e71a2d9b7a969ed23d673bb145dc18ef
MudOnTire <MudOnTire@MacBook-Pro.local>
2019-01-04 11:08:18 +0800

    finish api

---
+++ schema.js
@@ -0,0 +1,71 @@
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema } = require('graphql');
+const axios = require('axios');
+
+// Launch Type
+
+const LaunchType = new GraphQLObjectType({
+  name: 'Launch',
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_data_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  })
+});
+
+// Rocket Type
+
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    // launches
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios.get('https://api.spacexdata.com/v3/launches').then(res => res.data);
+      }
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`).then(res => res.data);
+      }
+    },
+    
+    // rockets
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios.get('https://api.spacexdata.com/v3/rockets').then(res => res.data);
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`).then(res => res.data);
+      }
+    },
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 
 const app = express();
 
-app.use('graphql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema,
-  graphql: true
+  graphiql: true
 }));
 
-app.listen(4000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
